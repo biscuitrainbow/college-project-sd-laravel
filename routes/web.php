e7ec80bc98be2dde6ef450a679aa8925f06c7ef7
Phuangchompoo Kumkrasae <mocomocopw@gmail.com>
2017-04-19 19:29:11 +0700

    เพิ่ม routes

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,75 @@ Route::get('/customer/display-result', function () {
     return view('customer.customer-display-result');
 });
 
+
+Route::get('/inquiry/change', function () {
+    return view('presale.inquiry.inquiry-change');
+});
+
+Route::get('/inquiry/change-result', function (){
+    return view('presale.inquiry.inquiry-change-result');
+});
+
+Route::get('/inquiry/create', function (){
+    return view('presale.inquiry.inquiry-create');
+});
+
+Route::get('/inquiry/delete', function (){
+    return view('presale.inquiry.inquiry-delete');
+});
+
+Route::get('/inquiry/display', function (){
+    return view('presale.inquiry.inquiry-display');
+});
+
+Route::get('/inquiry/display-result', function (){
+    return view('presale.inquiry.inquiry-display-result');
+});
+
+Route::get('/quotation/change', function (){
+    return view('presale.quotation.quotation-change');
+});
+
+Route::get('/quotation/change-result', function (){
+    return view('presale.quotation.quotation-change-result');
+});
+
+Route::get('/quotation/create', function (){
+    return view('presale.quotation.quotation-create');
+});
+
+Route::get('/quotation/delete', function (){
+    return view('presale.quotation.quotation-delete');
+});
+
+Route::get('/quotation/display', function (){
+    return view('presale.quotation.quotation-display');
+});
+
+Route::get('/quotation/display-result', function (){
+    return view('presale.quotation.quotation-display-result');
+});
+
+Route::get('/invoice/create', function (){
+    return view('payment.invoice.invoice-create');
+});
+
+Route::get('/invoice/display', function (){
+    return view('payment.invoice.invoice-display');
+});
+
+Route::get('/invoice/display-result', function (){
+    return view('payment.invoice.invoice-display-result');
+});
+
+Route::get('/receipt/create', function (){
+    return view('payment.receipt.receipt-create');
+});
+
+Route::get('/receipt/display', function (){
+    return view('payment.receipt.receipt-display');
+});
+
+Route::get('/receipt/display-result', function (){
+    return view('payment.receipt.receipt-display-result');
+});
