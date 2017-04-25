ef75b1b4a6117d396ec37dc00559f4069ad4f95b
yulgangau2 <alsomsau@gmail.com>
2017-04-25 21:16:22 +0700

    edit condition-display-general // ยังไม่ได้ทำserach

--- resources/views/condition/condition-display-general.blade.php
+++ resources/views/condition/condition-display-general.blade.php
@@ -3,7 +3,7 @@
 @section('breadcrumb')
     <a href="dashboard-overall.phpp" class="breadcrumb">Sale and Distribution</a>
     <a href="#!" class="breadcrumb">Condition</a>
-    <a href="{{route('displayMaterials')}}" class=" breadcrumb">General</a>
+    <a href="{{route('displayMaterials')}}" class=" breadcrumb">Material</a>
     <a href="{{route('displayMaterials')}}" class=" breadcrumb">Display</a>
     <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="{{route('logout')}}">Logout</a></li>
@@ -11,25 +11,67 @@
 @endsection
 
 @section('content')
-
-    <form action="">
+    <div id="mainApp">
+        <h4 style="margin-bottom: 50px">Display Material</h4>
+        <form action="">
+            <div v-model="query" class="input-field col s12 search-box">
+                <i class="material-icons prefix">search</i>
+                <input v-model="query" id="icon_prefix" type="text" class="validate">
+                <label for="icon_prefix">Search by product name or id</label>
+            </div>
+        </form>
         <div class="row">
-            <div class="input-field col s12">
-                <input value="{{$condition['conditionname']}}" id="conditionname" type="text" class="validate">
-                <label for="conditionname">Condition Name</label>
+            <div class="col s12">
+                <!-- Table -->
+                <table class="highlight">
+                    <thead>
+                    <tr>
+                        <th>Discount Name</th>
+                        <th>Discount Value</th>
+                        <th>Min Quantity</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <!-- <tr v-for="condition in search">  ยังเซิตไม่ได้
+                        <td>@{{condition.name}}</td>
+                        <td>@{{condition.discount}}</td>
+                        <td>@{{condition.min}}</td>
+                        <td><a :href="'{{url('/material/display')}}/' + condition.id"
+                               class="waves-effect waves-light btn">View</a>
+                        </td>
+                    </tr>-->
+                    @foreach($general as $gen)
+                    <tr>
+                            <td> {{ $gen->name }}</td>
+                            <td> {{  $gen->discount }}</td>
+                            <td> {{  $gen->min }}</td>
+                    </tr>
+                    @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
-        <div class="row">
-            <div class="input-field col s6">
-                <input value="{{$condition['discountprice']}}" id="discountprice" type="text" class="validate">
-                <label for="discountprice">Discount (%)</label>
-            </div>
-            <div class="input-field col s6">
-                <input value="{{$condition['minprice']}}" id="minprice" type="text" class="validate">
-                <label for="minprice">Min Price (ex. over 200,000 THB)</label>
-            </div>
-        </div>
-    </form>
+    </div>
+    <script src="{{asset('js/vue.js')}}"></script>
+   <!-- <script>
+        var mainApp = new Vue({
+            el: '#mainApp',
+            data: {
+                query: '' ,
+                conditions: {//!! $conditions->toJson() !!}
+            },
+            computed: {
+                search: function () {
+                    var self = this;
+                    if (this.query === '') {
+                        return this.conditions;
+                    }
+                    return this.conditions.filter(function (item) {
+                        return item.name.toUpperCase().indexOf(self.query.toUpperCase()) >= 0
+                    });
+                }
+            }
+        });
 
-
+    </script> -->
 @endsection
