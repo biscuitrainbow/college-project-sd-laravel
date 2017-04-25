6dc384c4943eee7c0da3740506f68a5589ef81a6
yakuzapai <yakuzapairock@gmail.com>
2017-04-26 02:28:26 +0700

    Prepare Goods Issue

--- app/Http/Controllers/GoodsIssueController.php
+++ app/Http/Controllers/GoodsIssueController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Document;
 use Illuminate\Http\Request;
-
+use DB;
 class GoodsIssueController extends Controller
 {
     public function create(){
-        return view('sale.goodsIssue.goodsissue-create');
+        $sale_orders = DB::select("
+        select documents.id,customers.company_name,documents.created_at,documents.request_date
+        from documents
+        join customers
+        on (documents.customer_id = customers.id)
+        where document_type_id = 4"
+        );
+        return view('sale.goodsIssue.goodsissue-create',compact('sale_orders'));
     }
 
     public function display(){
--- resources/views/sale/goodsIssue/goodsissue-create.blade.php
+++ resources/views/sale/goodsIssue/goodsissue-create.blade.php
@@ -2,29 +2,57 @@
 
 @section('breadcrumb')
   <a href="dashboard-overall.phpp" class="breadcrumb">Sale and Distribution</a>
-  <a href="#!" class="breadcrumb">Goods Issue</a>
-  <a href="{{route('displayMaterials')}}" class=" breadcrumb">Create</a>
+  <a href="#!" class="breadcrumb">Presale Process</a>
+  <a href="{{route('displayMaterials')}}" class=" breadcrumb">Quotation</a>
+  <a href="{{route('displayMaterials')}}" class=" breadcrumb">Display</a>
   <ul id="nav-mobile" class="right hide-on-med-and-down">
     <li><a href="{{route('logout')}}">Logout</a></li>
   </ul>
 @endsection
 
 @section('content')
-              <form action="">
-                <div class="row">
-                  <div class="input-field col s6">
-                    <input id="saleorderno" type="text" class="validate">
-                    <label for="saleorderno">Sale Order Number</label>
-                  </div>
-                  <div class="input-field col s6">
-                    <input type="date" class="datepicker">
-                    <label for="goodsissuedate">Goods Issue date</label>
-                  </div>
-                </div>
+  <h4 style="margin-bottom: 50px">Goods Issue</h4>
+  <form action="">
+    <div class="row">
+      <div class="input-field col s12">
+        <i class="material-icons prefix">search</i>
+        <input id="icon_prefix" type="text" class="validate">
+        <label for="icon_prefix">Search by product name or id</label>
+      </div>
+    </div>
+  </form>
+  <div class="card" style="margin-bottom:75px">
+    <div class="card-content">
+      <div class="row">
+        <div class="col s12">
 
-                <!-- Submit Button -->
-                <div class="row">
-                  <div class="col s12" style="text-align: right;"><a class="waves-effect waves-light btn">Create Goods Receipt Document</a></div>
-                </div>
-              </form>
-  @endsection
+          <!-- Table -->
+          <table class="highlight">
+            <thead>
+            <tr>
+              <th>Sale Order ID</th>
+              <th>Customer</th>
+              <th>Create Date</th>
+              <th>Request Delivery Date</th>
+              <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($sale_orders as $sale_order)
+              <tr>
+                <td>{{$sale_order->id}}</td>
+                <td>{{$sale_order->company_name}}</td>
+                <td>{{$sale_order->created_at}}</td>
+                <td>{{$sale_order->request_date}}</td>
+                <td><a href="/goodsissue/display/{{$sale_order->id}}" class="waves-effect waves-light btn">Select</a>
+                </td>
+              </tr>
+            @endforeach
+            </tbody>
+          </table>
+
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection()
