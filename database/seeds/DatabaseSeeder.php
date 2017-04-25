9b8bfed69d6f72eda5a3e98596f7f932340ab93f
Natthapon Sricort <Natthaponsricort@gmail.com>
2017-04-26 00:57:41 +0700

    Seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder {
         $this->call('ConditionTypeSeeder');
         $this->call('ConditionSeeder');
         $this->call('ConditionMaterialSeeder');
+        $this->call('DocumentSeeder');
+        $this->call('DocumentHasMaterialSeeder');
 
         $this->command->info('information seeded!');
     }
--- database/seeds/DocumentHasMaterialSeeder.php
+++ database/seeds/DocumentHasMaterialSeeder.php
@@ -1,16 +1,44 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
-class DocumentHasMaterialSeeder extends Seeder
-{
+class DocumentHasMaterialSeeder extends Seeder {
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run() {
+        DB::table('document_has_materials')->insert(array(
+            [
+                'document_id' => 1,
+                'material_id' => 1,
+                'quantity' => 300,
+                'created_at' => Carbon::now()->toDateTimeString(),
+                'updated_at' => Carbon::now()->toDateTimeString()
+            ],
+            [
+                'document_id' => 1,
+                'material_id' => 2,
+                'quantity' => 111,
+                'created_at' => Carbon::now()->toDateTimeString(),
+                'updated_at' => Carbon::now()->toDateTimeString()
+            ],
+            [
+                'document_id' => 1,
+                'material_id' => 3,
+                'quantity' => 120,
+                'created_at' => Carbon::now()->toDateTimeString(),
+                'updated_at' => Carbon::now()->toDateTimeString()
+            ],
+            [
+                'document_id' => 1,
+                'material_id' => 4,
+                'quantity' => 145,
+                'created_at' => Carbon::now()->toDateTimeString(),
+                'updated_at' => Carbon::now()->toDateTimeString()
+            ]
+        ));
     }
 }
