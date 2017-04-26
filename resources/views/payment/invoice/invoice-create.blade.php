8be86c00fdec5d4b449353329b37840c086abf53
yakuzapai <yakuzapairock@gmail.com>
2017-04-26 13:39:00 +0700

    เตรียม Invoice , Receipt

--- resources/views/payment/invoice/invoice-create.blade.php
+++ resources/views/payment/invoice/invoice-create.blade.php
@@ -2,36 +2,58 @@
 
 @section('breadcrumb')
     <a href="dashboard-overall.phpp" class="breadcrumb">Sale and Distribution</a>
-    <a href="#!" class="breadcrumb">Payment</a>
-    <a href="{{route('displayMaterials')}}" class=" breadcrumb">Invoice</a>
-    <a href="{{route('displayMaterials')}}" class=" breadcrumb">Create</a>
+    <a href="#!" class="breadcrumb">Presale Process</a>
+    <a href="{{route('displayMaterials')}}" class=" breadcrumb">Quotation</a>
+    <a href="{{route('displayMaterials')}}" class=" breadcrumb">Display</a>
     <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="{{route('logout')}}">Logout</a></li>
     </ul>
 @endsection
 
 @section('content')
+    <h4 style="margin-bottom: 50px">Create Invoice</h4>
     <form action="">
         <div class="row">
-            <div class="input-field col s6">
-                <input id="saleorderno" type="text" class="validate">
-                <label for="saleorderno">Sale Order Number</label>
+            <div class="input-field col s12">
+                <i class="material-icons prefix">search</i>
+                <input id="icon_prefix" type="text" class="validate">
+                <label for="icon_prefix">Search by product name or id</label>
             </div>
-            <div class="input-field col s6">
-                <input id="saleorderno" type="text" class="validate" placeholder="ex. cash, credit card, check note">
-                <label for="saleorderno">Term</label>
-            </div>
-        </div>
-        <div class="row">
-            <div class="input-field col s6">
-                <input type="date" class="datepicker">
-                <label for="goodsissuedate">Request Payment Date</label>
-            </div>
-        </div>
-
-        <!-- Submit Button -->
-        <div class="row">
-            <div class="col s12" style="text-align: right;"><a class="waves-effect waves-light btn">Create Invoice Document</a></div>
         </div>
     </form>
+    <div class="card" style="margin-bottom:75px">
+        <div class="card-content">
+            <div class="row">
+                <div class="col s12">
+
+                    <!-- Table -->
+                    <table class="highlight">
+                        <thead>
+                        <tr>
+                            <th>Sale Order ID</th>
+                            <th>Customer</th>
+                            <th>Create Date</th>
+                            <th>Request Delivery Date</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($good_issues as $good_issue)
+                            <tr>
+                                <td>{{$good_issue->id}}</td>
+                                <td>{{$good_issue->company_name}}</td>
+                                <td>{{$good_issue->created_at}}</td>
+                                <td>{{$good_issue->request_date}}</td>
+                                <td><a href="/invoice/create/{{$good_issue->id}}"
+                                       class="waves-effect waves-light btn">Select</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection()
